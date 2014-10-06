44a7e87bd0aa7123300f8e86e8818c0e83659cd0
Thibault Jouan <tj@a13.fr>
2014-10-06 22:21:52 +0000

    Add ssh_authorize macro

--- lib/producer/stdlib/ssh.rb
+++ lib/producer/stdlib/ssh.rb
@@ -3,13 +3,39 @@ module Producer
     module SSH
       require 'base64'
 
-      SSH_AUTHORIZED_KEYS_PATH = '.ssh/authorized_keys'.freeze
+      SSH_AUTHORIZED_KEYS_PATH  = '.ssh/authorized_keys'.freeze
+      SSH_AUTHORIZED_NO_KEY_FMT =
+        '"ssh_authorize macro cannot find key matching `%s\''
 
 
       STDLib.define_macro :ssh_dir do
         condition { no_dir? '.ssh' }
 
         mkdir '.ssh', 0700
+      end
+
+      STDLib.define_macro :ssh_authorize do |key_pattern, user: nil|
+        path_base = user ? '/home/%s' % user : nil
+        path      = Pathname.new(path_base) + SSH_AUTHORIZED_KEYS_PATH
+
+        condition { no_file? path }
+
+        if condition_met?
+          key = Net::SSH::Authentication::Agent.connect.identities.find do |e|
+            e.fingerprint =~ /\A#{key_pattern}/
+          end
+
+          fail Core::RuntimeError, SSH_AUTHORIZED_NO_KEY_FMT % key_pattern unless key
+
+          line = [
+            key.ssh_type,
+            ::Base64.encode64(key.to_blob).gsub("\n", '')
+          ].join ' '
+
+          ensure_dir path.dirname, 0700
+          file_write_once path, "#{line}\n", 0600
+          sh 'chown -R %s %s' % [user, path.dirname] if user
+        end
       end
 
       STDLib.define_macro :ssh_copy_id do |path = SSH_AUTHORIZED_KEYS_PATH|
