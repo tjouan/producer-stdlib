0c35b89354835c5d8b7181eaf8ae571aa30ac595
Thibault Jouan <tj@a13.fr>
2015-05-30 02:52:35 +0000

    Remove comments in JSON module

--- lib/producer/stdlib/json.rb
+++ lib/producer/stdlib/json.rb
@@ -4,9 +4,6 @@ require 'producer/stdlib/json/json_eq'
 module Producer
   module STDLib
     module JSON
-      #STDLib.define_test :json_eq do |path, data|
-      #  file_eq path, ::JSON.generate(data)
-      #end
       STDLib.define_test :json_eq, JSONEq
 
       STDLib.define_macro :json_write do |path, data|
